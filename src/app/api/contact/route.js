77d0bde57e49ae9b6fcf24d694e502f0bc585318
MatchbestGroup <matchbest17@gmail.com>
2025-07-26 10:05:39 +0530

    error

--- src/app/api/contact/route.js
+++ src/app/api/contact/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { db } from "../../../lib/firebase";
+import { db } from "../../lib/firebase";
 import { collection, addDoc, serverTimestamp } from "firebase/firestore";
 import { Resend } from "resend";
 
