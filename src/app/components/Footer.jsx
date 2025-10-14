cd2ccb1b1a3393c1082319192faaf571ac611628
MatchbestGroup <matchbest17@gmail.com>
2025-10-14 15:09:59 +0530

    Add TikTok and YouTube icons and links, update social media URLs

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,6 +1,6 @@
 'use client';
 import Link from 'next/link';
-import { FaFacebook, FaInstagram, FaLinkedin } from 'react-icons/fa';
+import { FaFacebook, FaInstagram, FaLinkedin, FaTiktok, FaYoutube } from 'react-icons/fa';
 
 export default function Footer() {
   return (
@@ -18,9 +18,11 @@ export default function Footer() {
         </div>
 
         <div className="flex justify-center space-x-6 text-2xl">
-          <a href="https://www.instagram.com/matchbest.software/" target="_blank" rel="noopener noreferrer" className="hover:text-gray-400"><FaInstagram /></a>
-          <a href="https://www.linkedin.com/company/matchbestllc" target="_blank" rel="noopener noreferrer" className="hover:text-gray-400"><FaLinkedin /></a>
-          <a href="#" target="_blank" rel="noopener noreferrer" className="hover:text-gray-400"><FaFacebook /></a>
+          <a href="https://www.instagram.com/matchbestgroup/" target="_blank" rel="noopener noreferrer" className="hover:text-gray-400"><FaInstagram /></a>
+          <a href="https://www.linkedin.com/company/matchbest-group/?viewAsMember=true" target="_blank" rel="noopener noreferrer" className="hover:text-gray-400"><FaLinkedin /></a>
+          <a href="https://www.facebook.com/profile.php?id=61581622601986" target="_blank" rel="noopener noreferrer" className="hover:text-gray-400"><FaFacebook /></a>
+          <a href="https://www.youtube.com/@matchbestgroup" target="_blank" rel="noopener noreferrer" className="hover:text-gray-400"><FaYoutube /></a>
+          <a href="https://www.tiktok.com/@matchbest.group?_t=ZS-90GhVEIK5nY&_r=1" target="_blank" rel="noopener noreferrer" className="hover:text-gray-400"><FaTiktok /></a>
         </div>
 
         <p className="text-sm">&copy; {new Date().getFullYear()} MatchBest. All rights reserved.</p>
@@ -28,4 +30,4 @@ export default function Footer() {
 
     </div>
   );
-} 
+}
