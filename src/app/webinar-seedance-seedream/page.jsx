09c992b4b56cd2bbf2747ec167a3dbf950c073a2
MatchbestGroup <matchbest17@gmail.com>
2025-10-31 18:15:53 +0530

    update homepage and rename event page to webinar

--- src/app/webinar-seedance-seedream/page.jsx
+++ src/app/webinar-seedance-seedream/page.jsx
@@ -37,10 +37,10 @@ export default function EventPage() {
       {/* HERO SECTION */}
       <section className="bg-gradient-to-r from-[#130F40] via-[#2C2C54] to-[#1B1464] text-white py-12 md:py-12 px-4 md:px-16 text-center ">
         <motion.div variants={fadeInUp} initial="hidden" animate="visible" className="max-w-7xl mx-auto mt-5 -mb-8">
-          <h1 className="text-2xl sm:text-4xl md:text-5xl font-extrabold mb-6 py-10">LIVE WEBINAR|Seedance & Seedream</h1>
-          <p className=" text-1xl md:text-lg text-gray-200 mb-4 md:mb-5 -mt-8">
-            Unlock next-generation strategies and insights for smarter, faster, AI-driven content creation.
-            <br className="hidden md:block"/>Join thought leaders in this exclusive webinar hosted by <span className="text-indigo-400 font-semibold text-2xl">Matchbest Group & BytePlus</span>.
+          <h1 className="text-2xl sm:text-4xl md:text-5xl font-extrabold mb-6 py-10">LIVE WEBINAR</h1>
+          <p className=" text-1xl md:text-2xl text-semibold text-gray-200 mb-4 md:mb-5 -mt-8">
+             Join thought leaders in this exclusive webinar hosted by <span className="text-indigo-400 font-semibold text-2xl">Seedance & Seedream</span>.        
+            <br className="hidden md:block"/> Unlock next-generation strategies and insights for smarter, faster, AI-driven content creation.
           </p>
           <p className="text-base md:text-lg text-gray-200 mb-4 md:mb-5">Your 1-hour shortcut to smarter strategies</p>
           <Link href="https://docs.google.com/forms/d/e/1FAIpQLScX2eGiZrmZCjbN1SSPGNYd9_voqLTmlpQOP2yMrMeX5W6ZHg/viewform" target="_blank" rel="noopener noreferrer">
