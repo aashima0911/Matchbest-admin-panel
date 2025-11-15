1d1744bd29d9ef00a9b418e5b08a6b2691678007
MatchbestGroup <matchbest17@gmail.com>
2025-11-15 14:13:31 +0530

    update Meta Pixel ID and remove duplicate tracking code
    - Change Facebook Pixel ID from '1396078278515018' to '630747470027499' in root layout
    - Remove redundant Meta Pixel Script and noscript tags from webinar landing page to eliminate duplication and streamline tracking configuration (the pixel is already included in the global layout)

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -111,7 +111,7 @@ export default function RootLayout({ children }) {
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '1396078278515018');
+            fbq('init', '630747470027499');
             fbq('track', 'PageView');
           `}
         </Script>
@@ -120,7 +120,7 @@ export default function RootLayout({ children }) {
             height="1"
             width="1"
             style={{ display: 'none' }}
-            src="https://www.facebook.com/tr?id=1396078278515018&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=630747470027499&ev=PageView&noscript=1"
             alt=""
           />
         </noscript>
--- src/app/matchbest-byteplus-webinar/page.jsx
+++ src/app/matchbest-byteplus-webinar/page.jsx
@@ -123,27 +123,7 @@ export default function WebinarLanding() {
 
   return (
     <>
-      {/* Meta Pixel Code */}
-      <Script id="meta-pixel-webinar" strategy="beforeInteractive">
-        {`
-          !function(f,b,e,v,n,t,s)
-          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-          n.queue=[];t=b.createElement(e);t.async=!0;
-          t.src=v;s=b.getElementsByTagName(e)[0];
-          s.parentNode.insertBefore(t,s)}(window, document,'script',
-          'https://connect.facebook.net/en_US/fbevents.js');
-          fbq('init', '630747470027499');
-          fbq('track', 'PageView');
-        `}
-      </Script>
-      <noscript>
-        <img height="1" width="1" style={{ display: 'none' }}
-        src="https://www.facebook.com/tr?id=630747470027499&ev=PageView&noscript=1"
-        />
-      </noscript>
-      {/* End Meta Pixel Code */}
+
       {/* Success Popup */}
       {showSuccessPopup && (
         <div className="fixed inset-0 bg-black bg-opacity-50 backdrop-blur-sm flex items-center justify-center z-50">
