d93a10cabf65e962462dd444061431e6941a21e1
matchbestsoftware1021-hub <matchbestsoftware1021@gmail.com>
2026-01-21 21:50:44 +0530

    solution pg

--- src/app/solutions/page.jsx
+++ src/app/solutions/page.jsx
@@ -46,41 +46,7 @@ const solutionsData = [
 const SolutionsPage = () => {
   return (
   <div className="bg-black text-white min-h-screen flex flex-col py-8 "> 
-  <section className="w-full pt-22 px-4 md:px-8 mb-2 ">
-    <div 
-      className="relative w-full max-w-[1300px] h-[150px] md:h-[200px] rounded-[3rem] overflow-hidden flex items-center justify-center shadow-2xl border border-white/5">
-
-      <div className="absolute inset-0 z-0" 
-        style={{
-        background: 'linear-gradient(60deg, #020010 0%, #0a0a4a 10%, #0055ff 25%, #7e22ce 40%, #000000 50%, #7e22ce 60%, #0055ff 75%, #0a0a4a 90%, #020010 100%)',
-        filter: 'blur(100px)', 
-        transform: 'scale(1.2)'
-        // style={{
-        // background: 'linear-gradient(60deg, #0b033f 0%, #030315 10%, #29354b 25%, #543e67 30%, #2a282d 50%, #5d4374 70%, #29354b 75%, #030315 90%, #030112 100%)',
-        // filter: 'blur(0px)', 
-        // transform: 'scale(1.2)'
-      }}> 
-
-      </div>
-      
-      {/* === CONTENT LAYER === */}
-      <div className="relative z-10 text-center px-6">
-        
-        {/* Main Heading */}
-        <h1 className="text-white text-4xl md:text-5xl font-['Inter'] font-normal mb-6 tracking-tight drop-shadow-lg">
-          Our Solutions
-        </h1>
-        
-        {/* Subheading */}
-        <p className="text-gray-200 text-md md:text-md font-['Inter'] font-light tracking-wide max-w-2xl mx-auto opacity-90">
-          Commit to provide excellent solutions
-        </p>
-
-      </div>
-
-    </div>
-
-
+  <section className="w-full px-4 md:px-8 mb-2 ">
     <div className="bg-black text-white min-h-screen flex flex-col ">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-20 ">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-8">
